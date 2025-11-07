a6602a742d0db371fb83cf2d4ffd5e57ccc7c807
Marco Antonio <141256155+MarcoZueiroBR@users.noreply.github.com>
2025-11-07 09:28:28 -0300

    Começo do Projeto
    Código em seu início para os membros do grupo já terem uma base, assim podendo idealizar algumas ideias próprias.

---
+++ Código/Transacao.java
@@ -0,0 +1,12 @@
+package Projeto_POO;
+
+public class Transacao {
+
+    private Pessoa cliente;
+    private double valor;
+
+    public Transacao(Pessoa cliente, double valor) {
+        this.cliente = cliente;
+        this.valor = valor;
+    }
+}
