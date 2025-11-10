0a255e0c97dc5bf28206a35349efe381e9453b65
Samuel <samuca12494@gmail.com>
2025-11-10 10:03:18 -0300

    Exceção de PontosInsuficientes

---
+++ Código/Exceptions/PontosInsuficientesException.java
@@ -0,0 +1,7 @@
+package Projeto_POO.Exceptions;
+
+public class PontosInsuficientesException extends RuntimeException {
+    public PontosInsuficientesException(String message) {
+        super(message);
+    }
+}
