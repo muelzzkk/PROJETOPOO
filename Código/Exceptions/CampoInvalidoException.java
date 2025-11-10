8f9fe3ad9d254f514e909c7ff56f800ef989d925
Samuel <samuca12494@gmail.com>
2025-11-10 09:54:23 -0300

    Criando Exceptions

---
+++ Código/Exceptions/CampoInvalidoException.java
@@ -0,0 +1,7 @@
+package Projeto_POO.Exceptions;
+
+public class CampoInvalidoException extends RuntimeException {
+  public CampoInvalidoException(String message) {
+    super(message);
+  }
+}
--- Código/Pessoa.java
+++ Código/Pessoa.java
@@ -11,4 +11,30 @@ public class Pessoa {
         this.cpf = cpf;
         this.email = email;
     }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
